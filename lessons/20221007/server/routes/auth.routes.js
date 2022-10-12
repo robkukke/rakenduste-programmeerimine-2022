5df6271b29df73d70ffaee43a9a40bf14d14bd11
robkukke <90237340+robkukke@users.noreply.github.com>
2022-10-12 22:00:14 +0300

    20221007 homework (wip)

--- lessons/20221007/server/routes/auth.routes.js
+++ lessons/20221007/server/routes/auth.routes.js
@@ -3,15 +3,39 @@
 const express = require("express")
 const router = express.Router()
 const authController = require("../controllers/auth.controller")
+const jwt = require("jsonwebtoken")
+const { body } = require("express-validator")
 
 // middleware that is specific to this router
-router.use((req, res, next) => {
-  console.log("Time: ", Date.now())
-  next()
-})
+const protectedMiddleware = (req, res, next) => {
+  if (req.headers["authorization"]) {
+    const token = req.headers.authorization.split(" ")[1]
+    jwt.verify(token, `${process.env.JWT_SECRET}`, (err, decoded) => {
+      if (err) return res.send(err)
+      req.user = {
+        name: decoded.name,
+        email: decoded.email,
+        status: decoded.status
+      }
+      next()
+    })
+  }
+}
 
-router.post("/signup", authController.signup)
-router.post("/login", authController.login)
-router.get("/protectedRoute", authController.protected)
+router.post(
+  "/signup",
+  body("name").not().isEmpty().trim().escape(),
+  body("email").isEmail().normalizeEmail(),
+  body("password").isLength({ min: 4 }),
+  authController.signup
+)
+router.post(
+  "/login",
+  body("name").not().isEmpty().trim().escape(),
+  body("email").isEmail().normalizeEmail(),
+  body("password").isLength({ min: 4 }),
+  authController.login
+)
+router.get("/protectedRoute", protectedMiddleware, authController.protected)
 
 module.exports = router
