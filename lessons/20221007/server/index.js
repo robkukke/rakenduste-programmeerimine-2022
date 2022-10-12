5ba6ad3f66e0bcab02d2994c86b7e1fa320950b1
robkukke <90237340+robkukke@users.noreply.github.com>
2022-10-12 18:43:01 +0300

    Create 20221007

---
+++ lessons/20221007/server/index.js
@@ -0,0 +1,41 @@
+// ES5 vs ES6 import
+const express = require("express")
+const morgan = require("morgan")
+const mongoose = require("mongoose")
+const app = express()
+const PORT = 8080
+require("dotenv").config()
+const authRoutes = require("./routes/auth.routes")
+
+app.use(morgan("dev"))
+app.use(express.json()) // body-parser asemel
+
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.gaw3wie.mongodb.net/?retryWrites=true&w=majority`
+mongoose
+  .connect(uri)
+  .then(() => console.log("Database connection established"))
+  .catch(e => console.error(e))
+
+app.use("/auth", authRoutes)
+
+// CRUD
+app.get("/", (req, res) => {
+  res.send("Hello World!")
+})
+
+app.get("/flights/:from-:to", (req, res) => {
+  console.log(req.body)
+
+  res.send({
+    params: req.params,
+    body: req.body
+  })
+})
+
+app.get("*", (req, res) => {
+  res.send("404")
+})
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`)
+})
